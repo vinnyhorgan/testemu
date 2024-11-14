fbed261f81dee91cdfd258e361258f3bd1d89fc2
Daniel Vincent Horgan <vinnyhorgan@gmail.com>
2024-11-14 19:03:40 +0100

    project setup

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-	printf("Hello\n");
+	if (!glfwInit()) {
+		return -1;
+	}
+
+	GLFWwindow *window = glfwCreateWindow(640, 480, "Test Emu - GL", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
