118019962f627b05a05a8f4bb1cf8340a8c74cae
Daniel Vincent Horgan <vinnyhorgan@gmail.com>
2024-11-15 20:07:25 +0100

    added miniaudio library

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#define MA_NO_DECODING
+#define MA_NO_ENCODING
+#define MA_NO_WAV
+#define MA_NO_FLAC
+#define MA_NO_MP3
+#define MA_NO_RESOURCE_MANAGER
+#define MA_NO_NODE_GRAPH
+#define MA_NO_ENGINE
+#define MA_NO_GENERATION
+#define MINIAUDIO_IMPLEMENTATION
+#include <miniaudio.h>
+
 #ifdef _WIN32
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3native.h>
