4120730d232d6fbd76e5030011a48607db27f6f8
Daniel Vincent Horgan <vinnyhorgan@gmail.com>
2024-11-16 12:18:25 +0100

    fixed linux build

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc -D_GLFW_X11 src/main.c src/lib/glad/src/*.c src/lib/glfw/src/*.c -o testemu -Isrc/lib/glad/include -Isrc/lib/glfw/include -lm
+gcc -D_GLFW_X11 src/main.c src/lib/glad/src/*.c src/lib/glfw/src/*.c src/lib/miniz/*.c -o testemu -Isrc/lib/glad/include -Isrc/lib/glfw/include -Isrc/lib/miniaudio -Isrc/lib/miniz -lm
